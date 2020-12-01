a37a8ec1cd9b9161b95a42934115c8811888c04e
ghozialghifari <44298867+ghozialghifari@users.noreply.github.com>
2020-12-01 19:24:01 +0700

    Add files via upload

---
+++ data/Titik_1.js
@@ -0,0 +1,8 @@
+var json_Titik_1 = {
+"type": "FeatureCollection",
+"name": "Titik_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 109.237825244106659, -7.418283953568124 ] } }
+]
+}
